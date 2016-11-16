29bda9643698f3b7141ef056512c2c403ba52bda
Max Jiang <maxjiang23326@gmail.com>
2016-11-16 23:58:00 +0800

    feat(snake): snake cannot reverse its direction

--- snake.js
+++ snake.js
@@ -3,18 +3,24 @@ const canvas = document.querySelector('#game-canvas'),
       w = canvas.width = 600,
       h = canvas.height = 405,
       r = 7.5,
-      MOVE_SPEED = 50,
+      MOVE_SPEED = 500,
       INIT_SNAKE_LENGTH = 5
 
 const LEFT_KEY = 97,
       UP_KEY = 119,
       RIGHT_KEY = 100,
-      DOWN_KEY = 115
+      DOWN_KEY = 115,
+      INIT_DIRECTION = RIGHT_KEY
 
 const direction$ = Rx.Observable.fromEvent(document, 'keypress')
   .map(e => e.keyCode)
   .filter(keyCode => [LEFT_KEY, UP_KEY, RIGHT_KEY, DOWN_KEY].includes(keyCode))
-  .startWith(RIGHT_KEY)
+  .scan((prev, curr) => {
+    const inSuccession = (...arr) => [prev, curr].every(_ => arr.includes(_))
+    return (inSuccession(LEFT_KEY, RIGHT_KEY) || inSuccession(UP_KEY, DOWN_KEY)) ? prev : curr
+  }, INIT_DIRECTION)
+  .distinctUntilChanged()
+  .startWith(INIT_DIRECTION)
 
 const snake$ = Rx.Observable.range(1, INIT_SNAKE_LENGTH)
   .map(() => ({ x: w / 2, y: h / 2 }))
@@ -22,8 +28,23 @@ const snake$ = Rx.Observable.range(1, INIT_SNAKE_LENGTH)
   .mergeMap(snake => Rx.Observable.interval(MOVE_SPEED)
     .withLatestFrom(direction$)
     .map(([i, direction]) => ({ direction, snake }))
-    .scan((prev, curr) => prev.map(move(curr.direction)), snake)
+    .scan(
+      (prev, curr) => ({
+        snake: prev.snake.map(move(prev.direction, curr.direction)),  // update each dot's position of the snake
+        direction: curr.direction
+      }),
+      { snake, direction: INIT_DIRECTION }
+    )
   )
+  .map(_ => _.snake)
+/*
+interval:   ---------0---------1---------2---------3---------
+direction$: R-----------U---------------------L--------------
+                             withLatestFrom
+            -------[0,R]-----[1,U]-----[2,U]-----[3,L]-------
+                                  map
+            -----{R,snake}-{U,snake}-{U,snake}-{L,snake}-----   
+*/
 
 const game$ = Rx.Observable.combineLatest(
     snake$,
@@ -31,33 +52,33 @@ const game$ = Rx.Observable.combineLatest(
       snake
     })
   )
-  .do(x => console.log(x))
+  // .do(x => console.log(x))
   .subscribe(renderSence)
 
-function move (direction) {
+function move (prevDirection, currDirection) {
   return dot => {
-    if (direction === LEFT_KEY) {
+    if (currDirection === LEFT_KEY) {
       return {
         x: dot.x - r * 2,
         y: dot.y
       }
     }
 
-    if (direction === RIGHT_KEY) {
+    if (currDirection === RIGHT_KEY) {
       return {
         x: dot.x + r * 2,
         y: dot.y
       }
     }
 
-    if (direction === UP_KEY) {
+    if (currDirection === UP_KEY) {
       return {
         x: dot.x,
         y: dot.y - r * 2
       }
     }
 
-    if (direction === DOWN_KEY) {
+    if (currDirection === DOWN_KEY) {
       return {
         x: dot.x,
         y: dot.y + r * 2
@@ -67,7 +88,7 @@ function move (direction) {
 }
 
 function renderSence (actors) {
-  ctx.clearRect(0, 0, w, h)
+  ctx.clearRect(0, 0, w, h)   // clear the canvas first
   renderSnake(actors.snake)
 }
 
