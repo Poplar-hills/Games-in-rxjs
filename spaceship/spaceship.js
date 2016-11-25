0279c83b2666cbc1a2721e69800cb994ea6b4a14
Max Jiang <maxjiang23326@gmail.com>
2016-11-25 23:28:02 +0800

    feat(spaceship): setup canvas

---
+++ spaceship/spaceship.js
@@ -0,0 +1,9 @@
+const SPEED = 40,
+      STAR_NUMBER = 250
+
+const canvas = document.querySelector('#game-canvas'),
+      ctx = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
