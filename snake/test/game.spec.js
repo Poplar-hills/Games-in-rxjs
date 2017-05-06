a2fcc288765d571625c2700eba93a29038f79cbb
Poplar_hills <maxjiang23326@gmail.com>
2017-05-06 15:51:28 +0800

    test(*): introduce ava

--- snake/test/game.spec.js
+++ snake/test/game.spec.js
@@ -1,17 +1,16 @@
-import {assert} from 'chai'
+import test from 'ava'
 import {Observable} from 'rxjs'
 import {TestScheduler} from 'rxjs/testing/TestScheduler'
 // import {getDirection$} from '../src/game'
 
-describe('Game', () => {
-  it('tring out TestScheduler', () => {
-    const scheduler = new TestScheduler(assert.deepEqual.bind(assert))
-    const up$ = scheduler.createHotObservable(  '--x---x--').mapTo('y')
-    const down$ = scheduler.createHotObservable('----x---x--')
-    const expected$ = '--y-x-y-x--'
-    scheduler.expectObservable(Observable.merge(up$, down$)).toBe(expected$)
-    scheduler.flush()
-  })
+test('tring out TestScheduler', t => {
+  const scheduler = new TestScheduler(t.deepEqual.bind(t))
+  const up$ = scheduler.createHotObservable(  '--x---x--').mapTo('y')
+  const down$ = scheduler.createHotObservable('----x---x--')
+  const expected$ = '--y-x-y-x--'
+  scheduler.expectObservable(Observable.merge(up$, down$)).toBe(expected$)
+  scheduler.flush()
+})
 
   // it('should get a stream of directions', () => {
   //   const scheduler = new TestScheduler(assert.deepEqual.bind(assert))
@@ -28,4 +27,4 @@ describe('Game', () => {
   //   scheduler.expectObservable(direction$).toBe(expected$, values)
   //   scheduler.flush()
   // })
-})
+// })
