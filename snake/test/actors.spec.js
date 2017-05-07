841605b4c5a3d3924e05cb38ab530d6605721fd9
Poplar_hills <maxjiang23326@gmail.com>
2017-05-07 23:04:12 +0800

    refactor(test): extract common parts

--- snake/test/actors.spec.js
+++ snake/test/actors.spec.js
@@ -13,16 +13,19 @@ const keypressValues = {
 }
 const directionValues = {W: 119, A: 97, S: 115, D: 100, T: 84, B: 66}
 
+const runTest = (scheduler, upStream, downStream, initDirection) => {
+  const keypress$ = scheduler.createHotObservable(upStream, keypressValues)
+  const direction$ = genDirection$(keypress$, initDirection)
+  scheduler.expectObservable(direction$).toBe(downStream, directionValues)
+  scheduler.flush()
+}
+
 test('direction$ should only emit the keycode values of W, A, S and D', t => {
   const scheduler = new TestScheduler(t.deepEqual.bind(t))
   const upStream =   '--S--T--D--B--T--W--'
   const downStream = '--S-----D--------W--'
   const initDirection = directionValues.D
-  const keypress$ = scheduler.createHotObservable(upStream, keypressValues)
-  const direction$ = genDirection$(keypress$, initDirection)
-
-  scheduler.expectObservable(direction$).toBe(downStream, directionValues)
-  scheduler.flush()
+  runTest(scheduler, upStream, downStream, initDirection)
 })
 
 test('direction$ should emit the first if two same directions occur in a row', t => {
@@ -30,11 +33,7 @@ test('direction$ should emit the first if two same directions occur in a row', t
   const upStream =   '--W--A--A--S--D--D--'
   const downStream = '--W--A-----S--D-----'
   const initDirection = directionValues.D
-  const keypress$ = scheduler.createHotObservable(upStream, keypressValues)
-  const direction$ = genDirection$(keypress$, initDirection)
-
-  scheduler.expectObservable(direction$).toBe(downStream, directionValues)
-  scheduler.flush()
+  runTest(scheduler, upStream, downStream, initDirection)
 })
 
 test('direction$ should omit the second dircetion if it is opposite to the initial direction', t => {
@@ -42,11 +41,7 @@ test('direction$ should omit the second dircetion if it is opposite to the initi
   const upStream =   '--A--S--'
   const downStream = '--D--S--'
   const initDirection = directionValues.D
-  const keypress$ = scheduler.createHotObservable(upStream, keypressValues)
-  const direction$ = genDirection$(keypress$, initDirection)
-
-  scheduler.expectObservable(direction$).toBe(downStream, directionValues)
-  scheduler.flush()
+  runTest(scheduler, upStream, downStream, initDirection)
 })
 
 test('direction$ should emit the first if two opposite directions occur in a row', t => {
@@ -54,9 +49,5 @@ test('direction$ should emit the first if two opposite directions occur in a row
   const upStream =   '--W--A--D--A--W--S--S--'
   const downStream = '--W--A--------W--------'
   const initDirection = directionValues.D
-  const keypress$ = scheduler.createHotObservable(upStream, keypressValues)
-  const direction$ = genDirection$(keypress$, initDirection)
-
-  scheduler.expectObservable(direction$).toBe(downStream, directionValues)
-  scheduler.flush()
+  runTest(scheduler, upStream, downStream, initDirection)
 })
